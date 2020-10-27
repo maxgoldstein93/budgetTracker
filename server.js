48cb24b2127354ba95a62f72def512b06aa73ad5
Max Goldstein <magoldstein93@gmail.com>
2020-10-27 09:29:56 -0400

    bugs

--- server.js
+++ server.js
@@ -3,19 +3,21 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3636;
+const PORT = process.env.PORT || 3636;
 
 const app = express();
 
 app.use(logger("dev"));
 
 app.use(compression());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+
 app.use(express.static("public"));
 // routes
 app.use(require("./routes/api.js"));
