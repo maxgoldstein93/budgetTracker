856d881e943cc6434bab0c661847cdce39624f99
Max Goldstein <magoldstein93@gmail.com>
2020-10-27 14:52:21 -0400

    bug with heroku

--- routes/api.js
+++ routes/api.js
@@ -4,9 +4,11 @@ const Transaction = require("../models/transaction.js");
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
+      console.log(dbTransaction)
       res.json(dbTransaction);
     })
     .catch(err => {
+      console.log(err)
       res.status(404).json(err);
     });
 });
